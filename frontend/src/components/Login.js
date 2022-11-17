93a9c5ec3c76de183a0f1aee0b959a520b4c53ba
Gian <guianpierodiaz@gmail.com>
2022-11-16 23:10:30 -0500

    Componentes

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+      <div className="container mt-3">
+        <h1>Login</h1>
+          <div className="mb-3 row">
+            <label for="staticEmail" className="col-sm-2 col-form-label">Email</label>
+            <div className="col-sm-10">
+                <input type='text' name='search' className='form-control'></input>
+            </div>
+        </div>
+        <div className="mb-3 row">
+            <label for="inputPassword" className="col-sm-2 col-form-label">Password</label>
+            <div className="col-sm-10">
+            <input type='password' name='search' className='form-control'></input>
+            </div>
+        </div>
+        
+      </div>
+    );
+  }
+  
+  export default Login;
