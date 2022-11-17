f176c8a88c4af6095d7936db7a6c8951822277bc
FabianLucana <70350793+FabianL5@users.noreply.github.com>
2022-11-17 00:06:22 -0500

    login-registro

--- frontend/src/components/registro.js
+++ frontend/src/components/registro.js
@@ -1,27 +1,45 @@
 import React from 'react';
 
-const registro = () => {
+const Registro = () => {
     return (
       <div className="container mt-3">
-        <h1>Inicio</h1>
-        <h1>Registro</h1>
+         <h1>Inicio</h1>
+        <h1>Login</h1>
           <div className="mb-3 row">
             <label for="staticEmail" className="col-sm-2 col-form-label">Nombre</label>
+            <div className="col-sm-10">
+                <input type='text' name='search' className='form-control'></input>
+            </div>
+        </div>
+
+        <div className="mb-3 row">
             <label for="staticEmail" className="col-sm-2 col-form-label">Apellido</label>
             <div className="col-sm-10">
                 <input type='text' name='search' className='form-control'></input>
             </div>
         </div>
+
         <div className="mb-3 row">
-            <label for="inputPassword" className="col-sm-2 col-form-label">Contraseña</label>
+            <label for="staticEmail" className="col-sm-2 col-form-label">DNI</label>
+            <div className="col-sm-10">
+                <input type='text' name='search' className='form-control'></input>
+            </div>
+        </div>
+        <div className="mb-3 row">
+            <label for="inputPassword" className="col-sm-2 col-form-label">Constraseña</label>
             <div className="col-sm-10">
             <input type='password' name='search' className='form-control'></input>
             </div>
         </div>
 
-        <div className='mt-3'>
-            <button className='btn btn-danger'>Olvide mi contraseña</button>
+        <div className="mb-3 row">
+            <label for="inputPassword" className="col-sm-2 col-form-label">Repetir constraseña</label>
+            <div className="col-sm-10">
+            <input type='password' name='search' className='form-control'></input>
+            </div>
         </div>
+
+    
         <div  className='mt-3'>
             <button className='btn btn-primary'>Registrarse</button>
         </div>
@@ -31,4 +49,4 @@ const registro = () => {
     );
   }
   
-  export default registro;
+  export default Registro;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Login from './components/Login';
-import registro from './components/registro';
+//import Login from './components/Login';
+import Registro from './components/Registro';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Login />
+    <Registro />
   </React.StrictMode>
 );
 
